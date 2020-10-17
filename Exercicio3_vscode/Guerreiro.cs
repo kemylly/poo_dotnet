19271cfc8fa0b7141ffea61dbe4e678760c0338d
Kemylly Cavalcante <55566427+kemylly@users.noreply.github.com>
2020-10-17 13:32:47 -0300

    Update Guerreiro.cs

--- Exercicio3_vscode/Guerreiro.cs
+++ Exercicio3_vscode/Guerreiro.cs
@@ -2,6 +2,26 @@ namespace Exercicio3
 {
     public class Guerreiro
     {
-        
+        private List<String> habilidade;
+        public List<String> Habilidade
+        {
+            get { return habilidade; }
+            set { habilidade = value; }
+        }
+
+        //metodos
+        public void attack()
+        {
+            
+        }
+        public override void LvUp()
+        {
+
+        }
+
+        public void AprenderHabilidade()
+        {
+
+        }
     }
 }
