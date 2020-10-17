73dc3971cb2d246c8e8c73812c6e1f88dee52ca7
Kemylly Cavalcante <55566427+kemylly@users.noreply.github.com>
2020-10-17 14:03:10 -0300

    adicionando construtores em mago

--- Exercicio3_vscode/Mago.cs
+++ Exercicio3_vscode/Mago.cs
@@ -2,7 +2,29 @@ namespace Exercicio3
 {
     public class Mago
     {
-         private List<String> magia;
+        public Mago():base()
+        {
+            Nome = "";
+            Vida = 0;
+            Mana = 0;
+            XP = 0;
+            Inteligencia =0;
+            Forca = 0;
+            Level = 0;
+        }
+
+        public Mago(String nome, int vida, int mana, float xp, int inteligencia, int forca, int level):base()
+        {
+            Nome = nome;
+            Vida = vida;
+            Mana = mana;
+            XP = xp;
+            Inteligencia = inteligencia;
+            Forca = forca;
+            Level = level;
+        }
+        
+        private List<String> magia;
         public List<String> Magia
         {
             get { return magia; }
