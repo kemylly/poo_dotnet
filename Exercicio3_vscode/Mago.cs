4285f6bcee5ced27319238b85cfb0caecba58ae1
Kemylly Cavalcante <55566427+kemylly@users.noreply.github.com>
2020-10-17 13:33:17 -0300

    Update Mago.cs

--- Exercicio3_vscode/Mago.cs
+++ Exercicio3_vscode/Mago.cs
@@ -2,6 +2,26 @@ namespace Exercicio3
 {
     public class Mago
     {
-        
+         private List<String> magia;
+        public List<String> Magia
+        {
+            get { return magia; }
+            set { magia = value; }
+        }
+
+        //metodos
+        public void attack()
+        {
+
+        }
+        public override void LvUp()
+        {
+
+        }
+
+        public void AprenderMagia()
+        {
+            
+        }
     }
 }
